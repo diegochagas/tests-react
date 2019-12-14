8e17890ee2729f8f557ba41f02b2df7449fdbf52
Diego Chagas <drochachagas@gmail.com>
2019-12-14 20:22:14 -0300

    useDispatch mock

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { render } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
 
+import { addTech } from '~/store/modules/techs/actions';
 import TechList from '~/components/TechList';
 
 jest.mock('react-redux');
@@ -12,10 +13,26 @@ describe('TechList component', () => {
       techs: ['Node.js', 'React.js']
     }));
 
-    const { getByTestId, getByText, debug } = render(<TechList />);
+    const { getByTestId, getByText } = render(<TechList />);
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
 
     expect(getByTestId('tech-list')).toContainElement(getByText('React.js'));
+  });
+
+  it('should be able to add new tech', () => {
+    const { getByTestId, getByLabelText } = render(<TechList />);
+
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
+
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' }} );
+
+    fireEvent.submit(getByTestId('tech-form'));
+
+    console.log(dispatch.mock.calls);
+
+    expect(dispatch).toHaveBeenCalledWith(addTech('Node.js'));
   });
 });
